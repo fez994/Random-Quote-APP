4ba4268d87a09c48acadf119dcc98d4ff8e8cc3c
Federico Dente <dentefederico@live.com>
2017-02-26 12:34:45 +0100

    added base structure and quotes

---
+++ js/index.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    
+
+
+var myQuotes = [
+
+{
+	quote: 'Education is like downloading data and algorithms into your brain',
+	author: 'Elon Musk'
+},
+
+{
+	quote: 'If you can explain the why of things, then that makes a huge difference to people’s motivation. Then they understand purpose',
+	author: 'Elon Musk'
+},
+
+{
+	quote: 'Starting a company is like starting into the abyss and eating glass',
+	author: 'Elon Musk'
+},
+
+{
+	quote: 'If something is important enough, even if the odds are against you, you should still do it',
+	author: 'Elon Musk'
+},
+
+
+{
+	quote:'Teachers open the door. You enter by yourself',
+	author: 'Chinese Proverb'
+},
+
+{
+	quote: 'If we Encounter a man of rare intellect, we should ask him what books he reads',
+	author: 'Ralph Waldo Emerson'	
+
+
+},
+
+
+{
+
+	quote: 'Strenght does not come from winning. Your struggles develop your strengths. When you go through hardships and decide not to surrender, that is strength.',
+	author: 'Arnold Schwarzenegger'
+},
+
+
+{
+	quote: 'For me life is continuosly being hungry. The meaning of life is not simply to exist, to survive, but to move ahed, to go up, to achieve, to conquer',
+	author: 'Arnold Schwarzenegger'
+
+}
+
+
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // end document ready function 
+
+
